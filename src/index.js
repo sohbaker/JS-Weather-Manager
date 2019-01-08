3951afc7eddd6cbdb85953253ac1d05867de1c53
Tola's Terminal <sohbaker@gmail.com>
2019-01-08 13:42:37 +0000

    refactored LondonOneDayWeather function. amended tests. refactored index.js to remove duplication

--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@ import { OneDayWeather } from './one_day_weather';
 const weatherOne = new OneDayWeather();
 
 async function getWeatherOne() {
-  const temp = document.getElementById('display-one-day-weather');
-  temp.innerHTML = await weatherOne.LondonOneDay() + '&deg;C';
-  return temp;
-};
-
-async function getDescriptionOne() {
-  const description = document.getElementById('display-one-day-description');
-  description.innerHTML = await weatherOne.LondonOneDayDescription();
-  return description;
-};
+  const displayTemp = document.getElementById('one-day-weather');
+  const displayDesc = document.getElementById('one-day-description');
+  const data = await weatherOne.LondonOneDayWeather();
+  displayTemp.innerHTML = data[0];
+  displayDesc.innerHTML = data[1];
+}
 
 getWeatherOne();
-getDescriptionOne();
