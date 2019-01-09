bf9d3a07537f86cac3b014ff36aeb9aa0f55e133
Tola's Terminal <sohbaker@gmail.com>
2019-01-09 15:53:45 +0000

    test passed: completes an weather api call for London for the current day

--- __tests__/api_test.js
+++ __tests__/api_test.js
@@ -12,4 +12,9 @@ describe('Api', () => {
   it('creates a new instance of Api', () => {
     expect(api instanceof Api).toEqual(true);
   });
+
+  it('completes an weather api call for London for the current day', async () => {
+    const weatherOneDay = await api.oneDayApiCall();
+    expect(weatherOneDay.list[0].name).toEqual('London');
+  });
 })
