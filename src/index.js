e5ef587beaa04f50136e8b6db1d6bb030424a749
Tola's Terminal <sohbaker@gmail.com>
2019-01-11 10:17:23 +0000

    created table, now names each day once with multiple forecasts for that day below

--- src/index.js
+++ src/index.js
@@ -19,37 +19,43 @@ async function getWeatherFive() {
 
 async function displayForecast() {
   const forecast = await getWeatherFive()
+
   const displayFourDays = document.getElementById('next-four-days')
+  let table = document.createElement("table")
 
   forecast.forEach((obj) =>  {
-    let info = document.createElement("div")
-
-    let day = document.createElement("p")
+    let day = document.createElement("tr")
     day.innerHTML = obj.day
-    info.appendChild(day);
+    table.appendChild(day);
 
-    let date = document.createElement("p")
+    let date = document.createElement("tr")
     date.innerHTML = obj.date
-    info.appendChild(date);
+    table.appendChild(date);
 
-    let time = document.createElement("p")
-    time.innerHTML = obj.time
-    info.appendChild(time);
+    let data = obj.data
+    let dataRow = document.createElement("tr")
 
-    let temp = document.createElement("p")
-    temp.innerHTML = obj.temp
-    info.appendChild(temp);
+      data.forEach((item) => {
+        let time = document.createElement("td")
+        time.innerHTML = item.time
+        dataRow.appendChild(time);
 
-    let icon = document.createElement("p")
-    let iconUrl = "http://openweathermap.org/img/w/" + obj.icon + ".png";
-    icon.innerHTML = "<img src='" + iconUrl  + "'>";
-    info.appendChild(icon);
+        let temp = document.createElement("td")
+        temp.innerHTML = item.temp
+        dataRow.appendChild(temp);
 
-    let desc = document.createElement("p")
-    desc.innerHTML = obj.desc
-    info.appendChild(desc);
+        let icon = document.createElement("td")
+        let iconUrl = "http://openweathermap.org/img/w/" + item.icon + ".png";
+        icon.innerHTML = "<img src='" + iconUrl  + "'>";
+        dataRow.appendChild(icon);
 
-    displayFourDays.appendChild(info);
+        let desc = document.createElement("td")
+        desc.innerHTML = item.desc
+        dataRow.appendChild(desc);
+      })
+
+    table.appendChild(dataRow);
+    displayFourDays.appendChild(table);
   })
 }
 
