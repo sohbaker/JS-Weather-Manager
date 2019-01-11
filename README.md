94b22d0f02ec21095025290daa857cf73a336e50
Tola's Terminal <sohbaker@gmail.com>
2019-01-11 17:23:56 +0000

    removing old tests from  weather_test.js after adding new tests which stub API calls

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Core Tasks
 [x] Get London's weather to display on a website using Open Weather Map https://openweathermap.org/api  
 [x] Show London's 5 day forecast on the page  
 [x] Abstract away API call logic into it's own class  
-- Stub out API calls in tests (Jest will help you here - https://jestjs.io/docs/en/tutorial-async)  
+[x] Stub out API calls in tests (Jest will help you here - https://jestjs.io/docs/en/tutorial-async)  
 - Deploy to Heroku  
 
 Stretch Goals  
--- __tests__/weather_test.js
+++ __tests__/weather_test.js
@@ -40,40 +40,4 @@ describe('Weather', () => {
     expect(mockFiveDayApiCall).toHaveBeenCalledTimes(1);
   });
 
-  // <-------------------------->
-  // let weather;
-  // beforeEach(() => {
-  //   weather = new Weather();
-  // });
-  //
-  // it('creates a new instance of Weather', () => {
-  //   expect(weather instanceof Weather).toEqual(true);
-  // });
-  //
-  // it('can access the temperature for the current weather in London', async () => {
-  //   const londonWeather = await weather.londonOneDayWeather();
-  //   expect(londonWeather[0]).toContain('\xB0');
-  // });
-  //
-  // it('can access the description for the current weather in London', async () => {
-  //   const londonWeather = await weather.londonOneDayWeather();
-  //   expect(typeof londonWeather[1]).toEqual('string');
-  // });
-  //
-  // it('can access the five day weather forecast for London', async () => {
-  //   const london5DayWeather = await weather.londonFiveDayWeather();
-  //   expect(london5DayWeather.cnt).toEqual(40);
-  // });
-  //
-  // it('adds a specific timestamp to each date for the next four days in a new array', async () => {
-  //   const timestamp = await weather.getDates();
-  //   expect(timestamp.length).toEqual(16);
-  // });
-  //
-  // it('collects the temperature, weather description and date/timestamp for the next four days', async () => {
-  //   const forecast = await weather.getForecast();
-  //   console.log(weather.getForecast())
-  //   expect(forecast.length).toEqual(4);
-  // });
-
 });
