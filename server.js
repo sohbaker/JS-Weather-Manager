206ce82c8260a59b43c95439ee514d5843b5dc88
Tola's Terminal <sohbaker@gmail.com>
2019-01-14 11:09:35 +0000

    attempt resolve .env

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-const key = require('dotenv').config();
 
 // /dist points to where main.js file is
 app.use(express.static(__dirname + '/dist'));
@@ -10,10 +9,6 @@ app.use(express.static(__dirname + '/dist'));
 // /dist/index points to where index.html is
 app.get('*',  (req, res) => {
   res.sendFile(path.resolve(__dirname, '/dist/index.html'));
-});
-
-let apiKey = new key.API_KEY({
-  accessKey: process.env.API_KEY
 });
 
 app.listen(port);
