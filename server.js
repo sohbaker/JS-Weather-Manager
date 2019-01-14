0d84d3601349cfd7c3d3d2addf91ac4a8cd402ff
Tola's Terminal <sohbaker@gmail.com>
2019-01-14 11:38:18 +0000

    amend require dotenv

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('*',  (req, res) => {
   res.sendFile(path.resolve(__dirname, './index.html'));
 });
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.API_KEY === 'production') {
   require('dotenv').config()
 };
 
